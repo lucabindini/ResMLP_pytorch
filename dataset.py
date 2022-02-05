fad144e6faa69a8e898a0327d7c10ea29831e067
lucabindini <lucabindini97@gmail.com>
2022-02-05 16:04:50 +0100

    Add model and testing notebook

---
+++ dataset.py
@@ -0,0 +1,27 @@
+import torchvision
+from torch.utils.data import DataLoader
+
+
+# Download training data from selected dataset
+training_data = torchvision.datasets.STL10(
+    root='datasets',
+    split='train',
+    download=True,
+    transform=torchvision.transforms.ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = torchvision.datasets.STL10(
+    root='datasets',
+    split='test',
+    download=True,
+    transform=torchvision.transforms.ToTensor(),
+) 
+
+batch_size = 64
+
+# Create training data loader
+train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+
+# Create test data loader
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
---
+++ learning_utils.py
@@ -0,0 +1,38 @@
+import torch
+
+
+def train(data, model, device, loss_fn, optimizer):
+
+    size = len(data.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(data):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f'loss: {loss:>7f}  [{current:>5d}/{size:>5d}] images trained')
+
+def test(data, model, device, loss_fn):
+
+    size = len(data.dataset)
+    num_batches = len(data)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in data:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f'Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n')
